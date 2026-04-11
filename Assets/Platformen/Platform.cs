6248af0768f4c6b3e2c0dfd60e5bb64b04dc6c42
Alexander Haas <haas.alexander110@gmail.com>
2026-04-11 13:56:28 +0200

    Platform change move to static start and endpoint

--- Assets/Platformen/Platform.cs
+++ Assets/Platformen/Platform.cs
@@ -5,21 +5,26 @@ public class Platform : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed;
-    public int startingPoint;
-    public Transform[] points;
+    public Transform startingPoint;
+    //public Transform[] points;
+    public bool isStart = true;
+
+    public Transform endpoint;
 
     private int i; //index of array
     private bool isMoving = false;
 
     void Start()
     {
-        transform.position = points[startingPoint].position;
+        transform.position = startingPoint.position;
     }
 
     void Update()
     {
         if (!isMoving) return;
 
+
+        /*
         if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
         {
             i++;
@@ -28,12 +33,16 @@ public class Platform : MonoBehaviour
                 i = 0;
             }
         }
+        */
+        //Vector2.Distance(transform.position, startingPoint.position) < 0.02f
 
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, !isStart ? endpoint.position : startingPoint.position, speed * Time.deltaTime);
+
     }
 
     public void startPlatform(){
         isMoving = true;
+        isStart = !isStart;
     }
 
     public void stopPlatform()
