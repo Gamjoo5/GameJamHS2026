3830437e4c74f3b6de264b2ea31b3d8c6fa9ec8a
Alexander Haas <haas.alexander110@gmail.com>
2026-04-12 14:19:39 +0200

    Heavy Bugs Fixed

--- Assets/Sprites/Player/PlayerAnimation.cs
+++ Assets/Sprites/Player/PlayerAnimation.cs
@@ -4,16 +4,19 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D rb;
+    private PlayerController2D controller;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<PlayerController2D>();
     }
 
     void Update()
     {
         animator.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
+        animator.SetBool("isHeavy", controller.IsHeavy);
 
         if (rb.linearVelocity.x > 0.1f)
             transform.localScale = new Vector3(-1, 1, 1);
